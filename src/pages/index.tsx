2be217ec83ad47fe4ec4c69372e001ffb12dc3f2
Douglas Soares <douglasmaismcpe@gmail.com>
2022-05-17 01:21:58 -0300

    Added new common styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,7 +53,7 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
             <a className={styles.post}>
               <strong>{post.data.title}</strong>
               <p>{post.data.subtitle}</p>
-              <div className={styles.postInfo}>
+              <div className={commonStyles.postInfo}>
                 <div>
                   <FiCalendar fontSize={20} />
                   <span>{post.first_publication_date}</span>
